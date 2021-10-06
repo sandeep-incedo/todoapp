a333c3d1a5741a1a98e72c247cef8840f098f795
sandeep-incedo <iamsandeep344@gmail.com>
2021-10-06 12:10:45 +0530

    Add files via upload

--- config/database.js
+++ config/database.js
@@ -1,1 +1,17 @@
+const mongoose = require('mongoose');
 
+const { MONGO_URI } = process.env;
+exports.connect = () => {
+    mongoose.connect(MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },)
+    .then( () => {
+        console.log('Successfully connected to database');
+    })
+    .catch(err => {
+        console.log('database connection fails. exiting now...');
+        console.log(err);
+        process.exit(1);
+    })
+}

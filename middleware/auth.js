b0521de7e110d0de3bdc2d42609a22a162a93cd9
sandeep-incedo <iamsandeep344@gmail.com>
2021-10-06 12:12:36 +0530

    Add files via upload

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,1 +1,24 @@
+const jwt = require("jsonwebtoken");
 
+const config = process.env;
+
+const verifyToken = (req, res, next) => {
+  const token =
+    req.body.token || req.query.token || req.headers["x-access-token"];
+    console.log("token");
+    console.log(token);
+  if (!token) {
+    return res.status(403).send("A token is required for authentication");
+  }
+  try {
+    const decoded = jwt.verify(token, config.TOKEN_KEY);
+    req.user = decoded;
+    //console.log(decoded);
+  } catch (err) {
+    console.log(err);
+    return res.status(401).send("Invalid Token");
+  }
+  return next();
+};
+
+module.exports = verifyToken;
